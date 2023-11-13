596bec1f5987290d3d832f14f32c473456558ecf
fgvieira <1151762+fgvieira@users.noreply.github.com>
2023-11-13 16:23:59 +0100

    Added option to filter LD estimates based on sites' MAF, and refactored logging

--- scripts/fit_LDdecay.R
+++ scripts/fit_LDdecay.R
@@ -1,18 +1,18 @@
 #!/bin/env Rscript
 
-#FileName: fit_LDdecay.R v1.0.9
-#Author: "Filipe G. Vieira (fgarrettvieira _at_ gmail [dot] com)"
+#FileName: fit_LDdecay.R v1.1.0
+#Author: "Filipe G. Vieira (fgvieira _at_ sund [dot] ku [dot] dk)"
 #Author: "Emma Fox (e.fox16 _at_ imperial [dot] ac [dot] uk)"
 
 ##IMPORTS
 library(optparse)
+library(lgr)
+library(jsonlite)
 library(tools)
-library(ggplot2)
+suppressMessages(library(ggplot2))
 library(reshape2)
 library(plyr)
 options(width = 200) 
-
-
 
 
 
@@ -21,10 +21,11 @@ option_list <- list(
   make_option(c('--ld_files'), action='store', type='character', default=NULL, help = 'File with list of LD files to fit and plot (if ommited, can be read from STDIN)'),
   make_option(c('--header'), action='store_true', type='logical', default=FALSE, help='Input file has header'),
   make_option(c('--col'), action='store', type='numeric', default=3, help='Which column is distance between sites? [%default]'),
-  make_option(c('--ld'), action='store', type='character', default="r2", help='Which LD stats to plot (r2pear, D, Dp, r2) [%default]'),
+  make_option(c('--ld'), action='store', type='character', default="r2", help='Which LD stats to plot (r2_ExpG, D, Dp, r2) [%default]'),
   make_option(c('--n_ind'), action='store', type='numeric', default=0, help='Number of individuals (for 1-parameter r^2 fitting correction)?'),
   make_option(c('-r', '--use_recomb_rate'), action='store_true', type='logical', default=FALSE, help='Assume constant recombination rate. [%default]'),
   make_option(c('--recomb_rate'), action='store', type='numeric', default=1, help='Recombination rate (or probability of recombination between adjacent sites in cM/Mb) to calculate genetic distances from physical distances. It is assumed to be constant throughout the whole dataset and, for human datasets, a common rule-of-thumb value is 1cM/Mb (1e-6). [%default]'),
+  make_option(c('--min_maf'), action='store', type='numeric', default=0, help='Minimum MAF of either site to include in the fitting analysis. [%default]'),
   make_option(c('--max_kb_dist'), action='store', type='numeric', default=Inf, help='Maximum distance between SNPs (in kb) to include in the fitting analysis. [%default]'),
   make_option(c('--fit_boot'), action='store', type='numeric', default=0, help='Number of bootstrap replicates for fitting CI. [%default]'),
   make_option(c('--fit_bin_size'), action='store', type='numeric', default=250, help='Bin data into fixed-sized windows for fitting. [default %default bps]'),
@@ -45,15 +46,18 @@ option_list <- list(
   make_option(c('-f','--plot_wrap_formula'), action='store', type='character', default=NULL, help='Plot formula for WRAP. [%default]'),
   make_option(c('-o','--out'), action='store', type='character', default=NULL, help='Output file'),
   make_option(c('--seed'), action='store', type='numeric', default=NULL, help='Seed for random number generator'),
-  make_option(c('--debug'), action='store_true', type='logical', default=FALSE, help='Debug mode. Extra output...')
+  make_option(c('-l','--loglevel'), action="store", type="character", default="info", help='Log verbosity level. [%default]')
 )
 opt_parser = OptionParser(option_list=option_list)
 opt = parse_args(opt_parser)
 
+# Set log
+lgr::threshold(opt$loglevel)
+
 # Set random seed
 if(is.null(opt$seed))
   opt$seed <- as.integer(runif(1,1,100000))
-cat("Random seed:", opt$seed, fill=TRUE)
+lgr$info("Random seed: %i", opt$seed)
 set.seed(opt$seed)
 
 # Parse input LD files
@@ -65,33 +69,32 @@ colnames(ld_files)[1] <- "File"
 for (id in colnames(ld_files))
   ld_files[,id] <- factor(ld_files[,id], unique(ld_files[,id]), ordered=TRUE)
 n_files <- nrow(ld_files)
-if(opt$debug)
-  print(ld_files)
+lgr$debug("Input files", data = toJSON(ld_files))
 
 # Parse LD stats to plot
 if(!is.null(opt$ld))
   opt$ld <- unlist(strsplit(opt$ld, ","))
-if(!all(opt$ld %in% c("r2pear", "D", "Dp", "r2")))
-  stop("Invalid LD measure to plot", call.=opt$debug)
+if(!all(opt$ld %in% c("r2_ExpG", "D", "Dp", "r2")))
+  stop(lgr$error("Invalid LD measure to plot"))
 n_ld = length(opt$ld)
 
 # Check if number of individuals was specified
 if(opt$n_ind < 0)
-  stop("Number of individuals must be greater than zero", call.=opt$debug)
+  stop(lgr$error("Number of individuals must be greater than zero"))
 if(opt$n_ind > 0 && length(unique(ld_files$File)) > 1)
-  warning("Sample size for correction will be assumed equal for all samples!", call.=opt$debug)
-if(!any(opt$ld %in% c("r2","r2pear")) && opt$n_ind)
-  stop("Number of individuals is only used for r^2 fitting", call.=opt$debug)
+  lgr$warn("Sample size for correction will be assumed equal for all samples!")
+if(!any(opt$ld %in% c("r2","r2_ExpG")) && opt$n_ind)
+  stop(lgr$error("Number of individuals is only used for r^2 fitting"))
 for(i in opt$ld)
-  cat("==> Fitting", i, "LD decay assuming a", ifelse(i == "Dp" || opt$n_ind == 0, "three (rate of decay, max LD and min LD)", "one (rate of decay)"), "parameter decay model", fill=TRUE)
+  lgr$info("Fitting %s LD decay assuming a %s parameter decay model", i, ifelse(i == "Dp" || opt$n_ind == 0, "three (rate of decay, max LD and min LD)", "one (rate of decay)"))
 
 # Check max_kb_dist parameter 
 if(opt$max_kb_dist < 50)
-  warning("Fitting of LD decay is highly unreliable at short distances (<50kb).", call.=opt$debug)
+  lgr$warn("Fitting of LD decay is highly unreliable at short distances (<50kb).")
 
 # Check binning sizes
 if(opt$plot_bin_size > 0 && opt$plot_bin_size < opt$fit_bin_size)
-   stop("Ploting bin size must be greater than fiting bin size!", call.=opt$debug)
+   stop(lgr$error("Ploting bin size must be greater than fiting bin size!"))
 
 # Parse plot_size
 opt$plot_size <- as.numeric(unlist(strsplit(opt$plot_size, ",")))
@@ -105,53 +108,57 @@ if(!is.null(opt$plot_wrap_formula))
 # Set output file name (if not defined)
 if(is.null(opt$out)){
   if(is.null(opt$ld_files))
-    stop('Output file name required, when reading LD files from STDIN')
+    stop(lgr$error('Output file name required, when reading LD files from STDIN'))
   opt$out <- paste(basename(file_path_sans_ext(opt$ld_files)),".pdf", sep = "")
 }
 
 
 ### Load LD data
-header <- c("Dist","r2pear","D","Dp","r2")
 ld_data <- data.frame()
 for (i in 1:n_files) {
   ld_file <- as.character(ld_files$File[i])
   # Read point data
-  if(opt$debug) cat("Reading file:", ld_file, fill=TRUE)
-  tmp_data <- read.table(gzfile(ld_file), sep="\t", quote="\"", dec=".")[-(1:(opt$col-1))]
+  lgr$info("Reading file %s", ld_file)
+  tmp_data <- read.table(gzfile(ld_file), sep="\t", header=TRUE, quote="\"", dec=".")
+  lgr$info("Number of lines: %i", nrow(tmp_data))
+  lgr$debug("Data", data = toJSON(head(tmp_data)))
   # Check if file is valid
   if(ncol(tmp_data) < 5)
-    stop('Invalid LD file format.\n', call.=opt$debug)
-  # Add column labels
-  colnames(tmp_data) <- header
+    stop(lgr$error('Invalid LD file format.'))
+  # Filter by minimum MAF
+  if ("maf1" %in% colnames(tmp_data) & "maf2" %in% colnames(tmp_data)) {
+    tmp_data <- tmp_data[which(tmp_data$maf1 >= opt$min_maf & tmp_data$maf2 >= opt$min_maf),]
+  }
+  # Filter by maximum distance
+  tmp_data <- tmp_data[which(tmp_data$dist < opt$max_kb_dist*1000),]
+  lgr$info("Number of lines after filtering: %i", nrow(tmp_data))
   # Extract relevant columns
-  tmp_data <- tmp_data[, which(names(tmp_data) %in% c("Dist",opt$ld))]
-  # Filter by minimum distance
-  tmp_data <- tmp_data[which(tmp_data$Dist < opt$max_kb_dist*1000),]
+  tmp_data <- tmp_data[, which(names(tmp_data) %in% c("dist",opt$ld))]
   # Convert all 'Inf' to NA
   tmp_data[mapply(is.infinite, tmp_data)] <- NA
   # Calculate genetic distances, according to Haldane's formula (assumes constant rate across all dataset)
   if(opt$use_recomb_rate && !is.null(opt$recomb_rate))
-    tmp_data$Dist <- (1 - (1 - opt$recomb_rate*0.01/1e6)^(tmp_data$Dist))/2
+    tmp_data$dist <- (1 - (1 - opt$recomb_rate*0.01/1e6)^(tmp_data$dist))/2
   # Bin data
   if(opt$fit_bin_size > 1) {
-    breaks <- seq(0, max(tmp_data$Dist)+opt$fit_bin_size, opt$fit_bin_size)
-    tmp_data$Dist <- cut(tmp_data$Dist, breaks, head(breaks, -1))
+    breaks <- seq(0, max(tmp_data$dist)+opt$fit_bin_size, opt$fit_bin_size)
+    tmp_data$dist <- cut(tmp_data$dist, breaks, head(breaks, -1))
     if(opt$bin_quant > 0) {
-      tmp_data <- aggregate(. ~ Dist, data=tmp_data, quantile, probs=opt$bin_quant/100, na.rm=TRUE)
+      tmp_data <- aggregate(. ~ dist, data=tmp_data, quantile, probs=opt$bin_quant/100, na.rm=TRUE)
     } else {
-      tmp_data <- aggregate(. ~ Dist, data=tmp_data, mean, na.rm=TRUE)
+      tmp_data <- aggregate(. ~ dist, data=tmp_data, mean, na.rm=TRUE)
     }
   }
   tmp_data$File <- ld_file
-  ld_data <- rbind(ld_data, melt(tmp_data, c("File","Dist"), variable.name="LD", na.rm=TRUE))
+  ld_data <- rbind(ld_data, melt(tmp_data, c("File","dist"), variable.name="LD", na.rm=TRUE))
 }
 # Clean-up
 rm(tmp_data)
-# Remove factor in Dist
-ld_data$Dist <- as.numeric(levels(ld_data$Dist))[ld_data$Dist]
+# Remove factor in dist
+ld_data$dist <- as.numeric(levels(ld_data$dist))[ld_data$dist]
 # Set maximum X-axis
 if(is.null(opt$plot_x_lim)) {
-  opt$plot_x_lim = max(ld_data$Dist)
+  opt$plot_x_lim = max(ld_data$dist)
 } else {
   opt$plot_x_lim = opt$plot_x_lim * 1000
 }
@@ -163,8 +170,7 @@ ld_data <- merge(ld_files, ld_data, by="File", sort=FALSE)
 # 
 n_groups <- length(unique(ld_data[,opt$plot_group]))
 n_plots <- n_files * n_ld / n_groups
-if(opt$debug)
-  print(head(ld_data))
+lgr$debug("Final data", data = toJSON(head(ld_data)))
 
 
 
@@ -172,7 +178,7 @@ if(opt$debug)
 # Model function
 ld_exp <- function(par, dist, ld_stat) {
   par <- as.numeric(par)
-  if(ld_stat == "r2" || ld_stat == "r2pear") {
+  if(ld_stat == "r2" || ld_stat == "r2_ExpG") {
     C = par[1] * dist
     r2h = par[2]
     r2l = par[3]
@@ -196,13 +202,10 @@ ld_exp <- function(par, dist, ld_stat) {
 # Evaluation function
 fit_eval <- function(par, obs_data) {
   if(length(unique(obs_data$LD)) != 1)
-    stop("Invalid data.frame (several LD measures)", call.=opt$debug)
-  model <- ld_exp(par, obs_data$Dist, obs_data$LD[1])
+    stop(lgr$error("Invalid data.frame (several LD measures)"))
+  model <- ld_exp(par, obs_data$dist, obs_data$LD[1])
   eval <- sum((model - obs_data$value)^2)
-  if(opt$debug) {
-    str(par)
-    print(eval)
-  }
+  lgr$trace("Fit evaluation", pars = par, eval = eval)
   eval
 }
 # Fitting function
@@ -219,17 +222,17 @@ fit_func <- function(x, fit_level) {
     if(ld_stat == 'Dp') {
       init_vals[1] = runif(1,10,20)
       par <- data.frame(init=init_vals, low_lim=c(0,0,0), up_lim=c(Inf,1,1))
-    } else { # r2 and r2pear
+    } else { # r2 and r2_ExpG
       init_vals[1] = runif(1,0,0.1)
       par <- data.frame(init=init_vals, low_lim=c(0,0,0), up_lim=c(1,1,1))
     }
-    if(opt$debug) str(par)
+    lgr$trace("Starting parameters", par = toJSON(par))
     
     optim_tmp <- append(optim_tmp, list("BFGS" = optim(par$init, fit_eval, obs_data=x, method="BFGS")) )
     if(fit_level > 1) optim_tmp <- append(optim_tmp, list("Nelder-Mead" = optim(par$init, fit_eval, obs_data=x, method="Nelder-Mead")) )
     if(fit_level > 2) optim_tmp <- append(optim_tmp, list("L-BFGS-B" = optim(par$init, fit_eval, obs_data=x, method="L-BFGS-B", lower=par$low_lim, upper=par$up_lim)) )
   }
-  if(opt$debug) str(optim_tmp)
+  lgr$debug("Optimization", data = toJSON(optim_tmp))
 
   # If not using the theoretical r2 decay curve (with r2h and r2l)
   if(opt$n_ind > 0)
@@ -245,8 +248,8 @@ fit_func <- function(x, fit_level) {
   
   # Pick best run
   optim_tmp <- optim_tmp[order(sapply(optim_tmp,'[[',2))[1]]
-  if(length(optim_tmp[[1]]) == 0) stop("convergence analyses failed. Please try increasing the fit level (--fit_level)", call.=opt$debug)
-  if(opt$debug) cat("Best fit for ", as.character(x$File[1]), " (",as.vector(ld_stat),"): ", names(optim_tmp), sep="", fill=TRUE)
+  if(length(optim_tmp[[1]]) == 0) stop(lgr$error("convergence analyses failed. Please try increasing the fit level (--fit_level)"))
+  lgr$debug("Best fit for %s (%s): %s", as.character(x$File[1]), as.vector(ld_stat), names(optim_tmp))
   optim_tmp[[1]]$par
 }
 
@@ -266,24 +269,23 @@ if(opt$fit_level > 0) {
     }
     boot_fit <- as.data.frame(as.matrix(aggregate(cbind(V1,V2,V3) ~ File+LD, boot_rep_fit, quantile, probs=c(0.025,0.975), names=FALSE)), stringsAsFactors=FALSE)
     optim_fit <- merge(optim_fit, boot_fit, sort=FALSE)
-    optim_data <- ddply(optim_fit, .(File,LD), function(x) data.frame(LD=x[,"LD"], Dist=smooth, value=ld_exp(x[,c("V1","V2","V3")], smooth, x[,"LD"]), ci_l=ld_exp(x[,c("V1.2","V2.1","V3.1")], smooth, x[,"LD"]), ci_u=ld_exp(x[,c("V1.1","V2.2","V3.2")], smooth, x[,"LD"])) )
+    optim_data <- ddply(optim_fit, .(File,LD), function(x) data.frame(LD=x[,"LD"], dist=smooth, value=ld_exp(x[,c("V1","V2","V3")], smooth, x[,"LD"]), ci_l=ld_exp(x[,c("V1.2","V2.1","V3.1")], smooth, x[,"LD"]), ci_u=ld_exp(x[,c("V1.1","V2.2","V3.2")], smooth, x[,"LD"])) )
   } else {
-    optim_data <- ddply(optim_fit, .(File,LD), function(x) data.frame(LD=x[,"LD"], Dist=smooth, value=ld_exp(x[,c("V1","V2","V3")], smooth, x[,"LD"])) )
+    optim_data <- ddply(optim_fit, .(File,LD), function(x) data.frame(LD=x[,"LD"], dist=smooth, value=ld_exp(x[,c("V1","V2","V3")], smooth, x[,"LD"])) )
   }
   # Print best FIT parameters
   optim_fit <- plyr::rename(optim_fit, c("V1"="DecayRate","V2"="LDmax","V3"="LDmin","V1.1"="DecayRate_CI.u","V1.2"="DecayRate_CI.l","V2.1"="LDmax_CI.l","V2.2"="LDmax_CI.u","V3.1"="LDmin_CI.l","V3.2"="LDmin_CI.u"), warn_missing=FALSE)
-  print(optim_fit)
+  lgr$info("Optimized fit", data = toJSON(optim_fit))
 
   # Merge data together with extra info from input
   fit_data <- merge(ld_files, optim_data, sort=FALSE)
-  if(opt$debug)
-    print(head(fit_data, n=10))
+  lgr$debug("Fit data", data = toJSON(head(fit_data, n=10)))
 }
 
 
 
 ### Create base plot
-cat("==> Plotting data...", fill=TRUE)
+lgr$info("Plotting data...")
 plot <- ggplot() + 
   theme(panel.spacing=unit(1,"lines")) +
   coord_cartesian(xlim=c(0,opt$plot_x_lim), ylim=opt$plot_y_lim) +
@@ -304,29 +306,28 @@ if(opt$fit_boot > 0) {
   grp <- NULL
   if(n_files == n_groups)
     grp <- opt$plot_group
-  plot <- plot + geom_ribbon(data=fit_data, aes_string(x="Dist",ymin="ci_l",ymax="ci_u",group=opt$plot_group,fill=grp), alpha=0.2)
+  plot <- plot + geom_ribbon(data=fit_data, aes(x=dist,ymin=ci_l,ymax=ci_u,group=.data[[opt$plot_group]],fill=.data[[grp]]), alpha=0.2)
 }
 
 # Add data points
 if(opt$plot_data){
   # Check format
   if(ncol(ld_data) < 4)
-    stop("Invalid `ld_data` format.\n", call.=opt$debug)
+    stop(lgr$error("Invalid `ld_data` format."))
   # Bin data
   if(opt$plot_bin_size > 1) {
-    breaks <- seq(0, max(ld_data$Dist)+opt$plot_bin_size, opt$plot_bin_size)
-    ld_data$Dist <- cut(ld_data$Dist, breaks, head(breaks, -1))
-    ld_data$Dist <- as.numeric(levels(ld_data$Dist))[ld_data$Dist]
+    breaks <- seq(0, max(ld_data$dist)+opt$plot_bin_size, opt$plot_bin_size)
+    ld_data$dist <- cut(ld_data$dist, breaks, head(breaks, -1))
+    ld_data$dist <- as.numeric(levels(ld_data$dist))[ld_data$dist]
     if(opt$bin_quant > 0) {
       ld_data <- aggregate(value ~ ., data=ld_data, quantile, probs=opt$bin_quant/100)
     } else {
       ld_data <- aggregate(value ~ ., data=ld_data, mean)
     }
   }
-  if(opt$debug)
-    print(head(ld_data, n=10))
+  lgr$debug("Plot data", data = toJSON(head(ld_data, n=10)))
   # Add points
-  plot <- plot + geom_point(data=ld_data, aes_string(x="Dist",y="value",colour=opt$plot_group), size=0.05, alpha=0.2)
+  plot <- plot + geom_point(data=ld_data, aes(x=dist,y=value,colour=.data[[opt$plot_group]]), size=0.05, alpha=0.2)
 }
 
 # Add LD decay best fit
@@ -334,23 +335,20 @@ if(length(opt$ld) > 0) {
   # Select fields that are variable
   header <- names(which(lapply(lapply(fit_data, unique), length) > 1))
   # Exclude non-relevant fields
-  grp <- header[!header %in% unique(c(as.character(opt$plot_wrap_formula), opt$plot_group, "Dist", "value", "File", "ci_l", "ci_u"))]
-  if(opt$debug) {
-    print(grp)
-    print(opt$plot_group)
-  }
+  grp <- header[!header %in% unique(c(as.character(opt$plot_wrap_formula), opt$plot_group, "dist", "value", "File", "ci_l", "ci_u"))]
+  lgr$debug("LD decay best fit", groups = grp, plot_groups = opt$plot_group)
   # Define line type
-  if(length(grp) == 0) grp <- NULL
-  if(length(grp) > 1) stop("invalid number of linetype groups!")
-  plot <- plot + geom_line(data=fit_data, aes_string(x="Dist",y="value",colour=opt$plot_group,linetype=grp))
+  if(length(grp) == 0) grp <- opt$plot_group
+  if(length(grp) > 1) stop(lgr$error("invalid number of linetype groups!"))
+  plot <- plot + geom_line(data=fit_data, aes(x=dist,y=value,colour=.data[[opt$plot_group]],linetype=.data[[grp]]))
   # If ploting data, add a thin black line to help see the line
   if(opt$plot_data && FALSE) # Disabled since, when plottig more than 1 line, it plots a grey area between them
-    plot <- plot + geom_line(data=fit_data, aes_string(x="Dist",y="value",colour=opt$plot_group), size=0.1, alpha=0.2, colour="black")
+    plot <- plot + geom_line(data=fit_data, aes(x=dist,y=value,colour=.data[[opt$plot_group]]), size=0.1, alpha=0.2, colour="black")
   # If plotting, apart from linetypes, also shapes (for B/W or color-blind printing)
   if(opt$plot_shape) {
     smooth <- seq(1, opt$plot_x_lim, length=opt$plot_line_smooth)[seq(2,opt$plot_line_smooth,length=5)]
-    sample_fit_data <- subset(fit_data, Dist %in% smooth)
-    plot <- plot + geom_point(data=sample_fit_data, aes_string(x="Dist",y="value",colour=opt$plot_group,shape=opt$plot_group))
+    sample_fit_data <- subset(fit_data, dist %in% smooth)
+    plot <- plot + geom_point(data=sample_fit_data, aes(x=dist,y=value,colour=.data[[opt$plot_group]],shape=.data[[opt$plot_group]]))
   }
 }
 
@@ -365,10 +363,8 @@ if(!is.null(opt$plot_wrap_formula)) {
 } else {
   par <- matrix(ncol=1)
 }
-if(opt$debug){
-  cat(n_files, n_ld, n_groups, n_plots, fill=TRUE)
-  cat(nrow(par), ncol(par), fill=TRUE)
-}
+lgr$debug("Summary", n_files = n_files, n_ld = n_ld, n_groups = n_groups, n_plots = n_plots)
+lgr$debug("Parameters layout", rows_par = nrow(par), cols_par = ncol(par))
 plot_height <- opt$plot_size[1] * nrow(par)
 plot_width <- opt$plot_size[2] * ncol(par)
 
@@ -385,4 +381,3 @@ if(n_groups < 2 || opt$plot_no_legend) {
 
 ### Save plot
 ggsave(opt$out, plot=plot, height=plot_height, width=plot_width, scale=opt$plot_scale, limitsize=FALSE)
-x <- warnings()
